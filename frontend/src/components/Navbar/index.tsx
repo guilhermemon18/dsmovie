4756cb6d378935685bc7152c42ce8b7b7a2d7bbf
guilhermemon18 <guilhermemonsani7@gmail.com>
2022-12-17 23:55:02 -0300

    arrumei  o link do instagram que eu esqueci

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar(){
         <nav className="container">
           <div className="dsmovie-nav-content">
             <h1>DSMOvie</h1>
-            <a href="https://github.com/devsuperior">
+            <a href="https://www.instagram.com/monsaniguilherme/">
               <div className="dsmovie-contact-container">
                 <GithubIcon />
                 <p className="dsmovie-contact-link"> /monsaniguilherme</p>
